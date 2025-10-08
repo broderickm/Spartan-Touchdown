818ac3f93b66b5ca9f92cd34108e315aaadc5e39
Sanika Kapre <kapresan@msu.edu>
2025-10-08 14:44:53 -0400

    added powerup class

---
+++ ProjectLib/PowerUp.cpp
@@ -0,0 +1,12 @@
+/**
+* @file PowerUp.cpp
+ * @author Sanika Kapre
+ *
+ * Class that describes power up
+ */
+
+using namespace std;
+
+#include "pch.h"
+
+#include "PowerUp.h"
---
+++ ProjectLib/PowerUp.h
@@ -0,0 +1,27 @@
+/**
+* @file PowerUp.h
+ * @author Sanika Kapre
+ *
+ * Class that describes power up
+ */
+
+#ifndef POWERUP_H
+#define POWERUP_H
+#include "MovingItem.h"
+
+/**
+ * Class that describes the power ups
+ */
+class PowerUp:public MovingItem{
+
+
+private:
+
+public:
+
+
+};
+
+
+
+#endif //POWERUP_H
