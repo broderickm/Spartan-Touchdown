bec22ce628a6b5f712f192f9ecc77aa3c29321ac
Sanika Kapre <kapresan@msu.edu>
2025-10-08 16:25:05 -0400

    added wall classes

---
+++ ProjectLib/BrickWall.cpp
@@ -0,0 +1,8 @@
+/**
+* @file BrickWall.cpp
+ * @author Sanika Kapre
+ *
+ * Class that describes brick wall
+ */
+
+#include "BrickWall.h"
---
+++ ProjectLib/BrickWall.h
@@ -0,0 +1,19 @@
+/**
+* @file BrickWall.h
+ * @author Sanika Kapre
+ *
+ * Class that describes brick wall
+ */
+
+#ifndef BRICKWALL_H
+#define BRICKWALL_H
+
+
+
+class BrickWall {
+
+};
+
+
+
+#endif //BRICKWALL_H
