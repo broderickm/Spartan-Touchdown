f5d0fcd515f5f98cd15c4d35e4ce3b24cd1471f7
Venkata Pandiri <Pandiriv@msu.edu>
2025-10-08 17:41:06 -0400

    Added the scoreBoard class,
    class is currently work in process.

---
+++ ProjectLib/ScoreBoard.cpp
@@ -0,0 +1,6 @@
+/**
+ * @file ScoreBoard.cpp
+ * @author Venkata Pandiri
+ */
+
+#include "ScoreBoard.h"
---
+++ ProjectLib/ScoreBoard.h
@@ -0,0 +1,20 @@
+/**
+ * @file ScoreBoard.h
+ * @author Venkata Pandiri
+ *
+ *
+ */
+
+#ifndef PROJECT1_SCOREBOARD_H
+#define PROJECT1_SCOREBOARD_H
+
+
+class ScoreBoard
+{
+private:
+
+public:
+};
+
+
+#endif //PROJECT1_SCOREBOARD_H
