62f23e1107214fb8b32b8e0799849754b4d6cea7
Venkata Pandiri <Pandiriv@msu.edu>
2025-10-09 23:08:12 -0400

    -added image loading for enemy class -added constructor for the enemy class -disabled assingment operator, copy constructor, and default constructor

--- ProjectLib/Enemy.cpp
+++ ProjectLib/Enemy.cpp
@@ -5,3 +5,18 @@
 
 #include "pch.h"
 #include "Enemy.h"
+
+
+using namespace std;
+
+
+
+/**
+ * Constructor
+ * @param game, the game we are adding this enemy to
+ * @param filename Filename for the image we use
+ */
+Enemy::Enemy(Game* game, const std::wstring& filename): MovingItem(game,filename)
+{
+
+}
--- ProjectLib/Enemy.h
+++ ProjectLib/Enemy.h
@@ -18,6 +18,15 @@ private:
 
 
 public:
+    /// default constructor disabled
+    Enemy() = delete;
+    /// default copy constructor disabled
+    Enemy(const Enemy &) = delete;
+    /// = copy operator disabled
+    void operator=(const Enemy &) = delete;
+
+    /// note: this might be a protected member, for now I am making it public
+    Enemy(Game * game, const std::wstring& filename);
 };
 
 
